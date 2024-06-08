7c73589d006c6f75f0e786840fc6e4ae24a9447e
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-06-08 17:10:13 -0500

    able to edit posts, working on submitting edits properly

--- src/components/DisplayPost.jsx
+++ src/components/DisplayPost.jsx
@@ -1,69 +1,114 @@
 import CreatePost from "./CreatePost.jsx";
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-import Post from "./Post.jsx";
 
 function DisplayPost({ posts }) {
-	const [published, setPublished] = useState();
-	const [editing, setEditing] = useState(false);
-	// const [activeIndex, setActiveIndex] = useState();	
+	console.log(posts, "this is posts");
+	return (
+		<div>
+			{posts &&
+				posts.map((post, index) => (
+					<Post
+						key={index}
+						title={post.title}
+						date={post.date}
+						name={post.user.name}
+						text={post.text}
+						visibility={post.visibility}
+					/>
+				))}
+			<CreatePost />
+		</div>
+	);
+}
 
-	let viewMode = {};
-	let editMode = {};
-	if (editing) {
-		viewMode.display = "none";
-	} else {
-		editMode.display = "none";
-	}
+function Post({ title, date, name, text, visibility }) {
+	const [editing, setEditing] = useState(false);
+	const [published, setPublished] = useState(visibility);
+	const [formData, setFormData] = useState({
+		title: title,
+		text: text,
+	});
 
 	const checkHandler = () => {
 		setPublished(!published);
 	};
-	const handleEditing = (index) => {
-		// setActiveIndex(index);
-		const newPosts = [...posts];
-		const updatePosts = newPosts[index];
+
+	const handleChange = (event) => {
+		setFormData(event.target.value);
+	};
+
+	const handleEditing = () => {
 		setEditing(!editing);
 	};
 
-	return (
-		<div>
-			{posts &&
-				posts.map((post, index) => {
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		console.log(formData, "this is formdata");
+		//have form data, take the title and such and send to backend
+		const JWTToken = localStorage.getItem("JWT Token");
+		const formDataTitle = formData.title;
+		const formDataText = formData.text;
 
-					return (
-						<div key={uuidv4()}>
-							<ul style={viewMode}>
-								<li>{post.title}</li>
-								<li>{post.date}</li>
-								<li>{post.user.name}</li>
-								<li>{post.text}</li>
-								<li>
-									<input
-										type="checkbox"
-										checked={published}
-										onChange={checkHandler}
-									/>
-									{post.visibility}
-								</li>
-								<button
-									onClick={() => {
-										handleEditing(index);
-									}}
-								>
-									Edit
-								</button>
-							</ul>
-							<ul style={editMode}>
-								<CreatePost value={post.title} posts={posts} />
-								<button onClick={handleEditing}>Edit</button>
-							</ul>
-						</div>
-					);
-				})}
-			<CreatePost />
-			<Post />
-		</div>
+		const isPublished = event.target.elements[2].checked
+
+		fetch("http://localhost:3000/post", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				formDataTitle,
+				JWTToken,
+				formDataText,
+				isPublished
+			}),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				return data;
+			});
+	};
+
+	return (
+		<>
+			{editing ? (
+				<form action="POST" onSubmit={handleSubmit}>
+					<input
+						type="text"
+						name="title"
+						value={formData.title}
+						onChange={handleChange}
+					/>
+					<input
+						type="text"
+						name="text"
+						value={formData.text}
+						onChange={handleChange}
+					/>
+					<input
+						type="checkbox"
+						onChange={checkHandler}
+						value={published}
+					/>
+					Publish
+					<button type="submit">Add Post</button>
+					{/* <button onClick={handleSubmit}>Submit</button> */}
+				</form>
+			) : (
+				<>
+					<li>{title}</li>
+					<li>{date}</li>
+					<li>{name}</li>
+					<li>{text}</li>
+					<li>{visibility}</li>
+					<input
+						type="checkbox"
+						checked={published}
+						onChange={checkHandler}
+					/>
+					<button onClick={handleEditing}>Edit</button>
+				</>
+			)}
+		</>
 	);
 }
 
