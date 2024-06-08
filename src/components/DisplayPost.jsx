cb9e42558218b7fc0b365b28cea37e9e743612b2
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-06-08 14:52:55 -0500

    update for question

--- src/components/DisplayPost.jsx
+++ src/components/DisplayPost.jsx
@@ -1,13 +1,12 @@
 import CreatePost from "./CreatePost.jsx";
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-import FetchPost from "./FetchPost";
+import Post from "./Post.jsx";
 
 function DisplayPost({ posts }) {
-	
 	const [published, setPublished] = useState();
 	const [editing, setEditing] = useState(false);
-	// const [activeIndex, setActiveIndex] = useState();
+	// const [activeIndex, setActiveIndex] = useState();	
 
 	let viewMode = {};
 	let editMode = {};
@@ -20,47 +19,50 @@ function DisplayPost({ posts }) {
 	const checkHandler = () => {
 		setPublished(!published);
 	};
-	const handleEditing = () => {
+	const handleEditing = (index) => {
 		// setActiveIndex(index);
+		const newPosts = [...posts];
+		const updatePosts = newPosts[index];
 		setEditing(!editing);
 	};
+
 	return (
 		<div>
 			{posts &&
-				posts.map((item) => {
+				posts.map((post, index) => {
+
 					return (
 						<div key={uuidv4()}>
-							<ul
-								style={viewMode}
-							>
-								<li>{item.title}</li>
-								<li>{item.date}</li>
-								<li>{item.user.name}</li>
-								<li>{item.text}</li>
+							<ul style={viewMode}>
+								<li>{post.title}</li>
+								<li>{post.date}</li>
+								<li>{post.user.name}</li>
+								<li>{post.text}</li>
 								<li>
 									<input
 										type="checkbox"
 										checked={published}
 										onChange={checkHandler}
 									/>
-									{item.visibility}
+									{post.visibility}
 								</li>
-								<button onClick={handleEditing}>
+								<button
+									onClick={() => {
+										handleEditing(index);
+									}}
+								>
 									Edit
 								</button>
 							</ul>
-							<ul
-								style={editMode}
-							>
-								<CreatePost value={item.title} posts={posts} />
-								<button onClick={handleEditing}>
-									Edit
-								</button>
+							<ul style={editMode}>
+								<CreatePost value={post.title} posts={posts} />
+								<button onClick={handleEditing}>Edit</button>
 							</ul>
 						</div>
 					);
 				})}
 			<CreatePost />
+			<Post />
 		</div>
 	);
 }
